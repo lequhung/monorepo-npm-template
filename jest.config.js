735d2708ff71a058a1866de7670d288636ecaca2
Hung Le <hungqule@gmail.com>
2023-11-07 00:30:22 +1100

    updated jest config

--- jest.config.js
+++ jest.config.js
@@ -1,27 +1,13 @@
 module.exports = {
-  displayName: 'broccoli-and-co',
-  testEnvironmentOptions: {
-    url: 'http://localhost:3000'
-  },
-  roots: ['<rootDir>/src'],
-  coverageThreshold: {
-    global: {
-      statements: 10,
-      branches: 10,
-      functions: 10,
-      lines: 10
+  verbose: true,
+  projects: [
+    {
+      displayName: 'a'
+      // configs for package a
+    },
+    {
+      displayName: 'b'
+      // configs for package b
     }
-  },
-  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
-  coveragePathIgnorePatterns: ['<rootDir>/src/index.tsx'],
-  setupFilesAfterEnv: ['<rootDir>/jest/setupTests.ts'],
-  moduleFileExtensions: ['tsx', 'ts', 'js', 'json'],
-  testMatch: ['<rootDir>/src/**/__test?(s)__/**/*.test.ts?(x)'],
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.(tsx?)$': 'ts-jest'
-  },
-  reporters: ['default', ['jest-junit', { outputName: 'jest-report.xml' }]],
-  preset: 'ts-jest',
-  verbose: true
+  ]
 };
